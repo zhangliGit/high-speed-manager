99f22221e56a6b34af4066face0eaf180ec331ed
zhangliGit <674937076@qq.com>
2018-11-17 08:43:11 +0800

    xx

--- src/views/App.js
+++ src/views/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import Container from './Container'
 import TicketList from './Ticket/TicketList'
 import TicketDetail from './Ticket/TicketDetail'
-import { CSSTransition } from 'react-transition-group'
 import store from '../store/index';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -15,27 +14,9 @@ export default class App extends React.Component {
       <Provider store = { store }>
         <Router>
             <Switch>
-            <CSSTransition
-              in = { this.props.match !== null }
-              classNames = "slide-in"
-              timeout = { 300 }
-            >
               <Route exact path = "/" component = { Container } />
-            </CSSTransition>
-            <CSSTransition
-              in = { this.props.match !== null }
-              classNames = "slide-in"
-              timeout = { 300 }
-            >
               <Route exact path = "/TicketList" component = { TicketList } />
-            </CSSTransition>
-            <CSSTransition
-              in = { this.props.match !== null }
-              classNames = "slide-in"
-              timeout = { 300 }
-            >
               <Route exact path = "/TicketDetail" component = { TicketDetail } />
-            </CSSTransition>
             </Switch>
         </Router>
       </Provider>
